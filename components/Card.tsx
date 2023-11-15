2487316e78791c8a1512c54e52aa0585eafbcd72
zeph1re <arlinnog1122@gmail.com>
2023-11-15 21:06:24 +0700

    add project to project list

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,12 +6,12 @@ import { CardProps } from '@/types';
 function Card({ imageLink, projectTitle, projectDescription, link }: CardProps) {
   return (
     <div className="project_card">
-      <Image src={`${imageLink}`} alt="rinengga app" width={300} height={300} className="mx-auto" />
+      <Image src={`${imageLink}`} alt="rinengga app" width={200} height={200} className="mx-auto" />
       <div className="my-auto">
         <h1 className="project_title">{projectTitle}</h1>
         <p className="project_description">{projectDescription}</p>
         <div className="flex justify-center">
-          <CustomButton title="Check Github" containerStyles="flex bg-grey text-white rounded-full mt-5 " link={link} />
+          <CustomButton title="View Project" containerStyles="flex bg-grey text-white rounded-full mt-5 " link={link} />
         </div>
       </div>
     </div>
--- components/Work.tsx
+++ components/Work.tsx
@@ -1,21 +1,39 @@
-import Image from 'next/image';
-import React from 'react';
-import { CustomButton } from '.';
+'use client';
+
+import { useState } from 'react';
 import Card from './Card';
 
 function Work() {
-  const project = [{}, {}];
+  const [projects, setProjects] = useState([]);
+
+  const project = [
+    {
+      title: 'Rinengga App',
+      desc: 'This is an Education Application that uses Kotlin and Firebase for the core. I developed this app from scratch like User Research, Designing with Figma, Development, and Testing.',
+      imageLink: '/rinengga_app.png',
+      url: 'https://github.com/zeph1re/RinenggaApp',
+    },
+    {
+      title: 'SecondHand',
+      desc: 'This is an E-Commerce Application using an Android Platform named SecondHand that is held by Binar Academy for graduation requirements. In this application, we can buy and sell second products that we used before. This platform provides various types of user needs. This platform will bring together sellers and buyers to be able to negotiate goods and conduct transactions directly outside the platform',
+      imageLink: '/second_hand_splash.png',
+      url: 'https://github.com/zeph1re/SecondHand5',
+    },
+    {
+      title: 'Hotel App',
+      desc: 'Dicoding Final Project Belajar Membuat Aplikasi Flutter untuk Pemula, Hotel App for Hotel Reservation',
+      imageLink: '/hotel_app.png',
+      url: 'https://github.com/zeph1re/HotelApp',
+    },
+  ];
 
   return (
     <div className="work">
-      <h1 className="work_title"> PERSONAL PROJECT </h1>
+      <h1 className="work_title"> MY PROJECT </h1>
       <div className="project_container">
-        <Card
-          imageLink="/rinengga_app.png"
-          projectTitle="Rinengga App"
-          projectDescription="This is an Education Application that uses Kotlin and Firebase for the core. I developed this app from scratch like User Research, Designing with Figma, Development, and Testing."
-          link="https://github.com/zeph1re/RinenggaApp"
-        ></Card>
+        {project.map((project, index) => {
+          return <Card imageLink={project.imageLink} projectTitle={project.title} projectDescription={project.desc} link={project.url} />;
+        })}
       </div>
     </div>
   );
